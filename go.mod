4c94e96fac064f1ed2e7b171a834b28abe88f59a
johnservo <johnservo.se@gmail.com>
2024-07-18 00:04:55 +0800

    initial script

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JohnServo24/repo-autopusher
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	homeDir := os.Getenv("HOME")
+	if homeDir == "" {
+		log.Fatal("No $HOME env")
+	}
+
+	targetDir := filepath.Join(homeDir, "Repos", "obsidian-notes") // One repo for now
+
+	if err := os.Chdir(targetDir); err != nil {
+		log.Fatal("Error changing directory:", err)
+	}
+
+	fmt.Println("Current Directory: ", targetDir)
+
+	cmd := exec.Command("git", "add", ".")
+	_, err := cmd.Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	cmd = exec.Command("git", "commit", "-m", currentTime+" - Auto pushed")
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(stdout))
+
+	cmd = exec.Command("git", "push")
+	stdout, err = cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(stdout))
+
+	fmt.Println("======================")
+	fmt.Println("Successfully pushed the repos")
+}
